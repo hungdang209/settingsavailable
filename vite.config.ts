dfd2ac7813abda7599a4ce4f29ef1e16bddc18b6
bet999 <999bestbet@gmail.com>
2024-09-19 11:53:07 +0700

    upp

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/theme-number-ten/",
   resolve: {
     alias: {
       Components: "/src/components",
