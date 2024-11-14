126bedd06ab6b8ce51b17e3b1d180720690a899d
buckridgelucy123 <buckridgelucy123@gmail.com>
2024-11-14 09:32:31 +0700

    a

--- src/components/contents/ContentComponent.tsx
+++ src/components/contents/ContentComponent.tsx
@@ -1275,20 +1275,7 @@ const ContentComponent = (props: Props) => {
                                         </Form.Item>
                                     </div>
 
-                                    <div className="item-form">
-                                        <label>Link to fanpage</label>
-                                        <Form.Item
-                                            name="fanpageName"
-                                            rules={[
-                                                {
-                                                    required: true,
-                                                    message: 'Please input link to fanpage!',
-                                                }
-                                            ]}
-                                        >
-                                            <Input />
-                                        </Form.Item>
-                                    </div>
+                                    
 
                                     <div className="item-form">
                                         <Form.Item
